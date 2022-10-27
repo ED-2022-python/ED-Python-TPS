81493f608f928a777a6c24bba37246f539d771eb
Ramiro Buenar <ramirobuenar2000@gmail.com>
2022-10-26 22:00:55 -0300

    Agregadas todas las clases de los ejercicios

---
+++ Ejercicio3_4/UnsortedPriorityQueue.py
@@ -0,0 +1,21 @@
+from typing import Tuple, Any
+
+from UnsortedPriorityQueueAbstract import UnsortedPriorityQueueAbstract
+
+
+class UnsortedPriorityQueue(UnsortedPriorityQueueAbstract):
+    def __len__(self) -> int:
+        pass
+
+    def is_empty(self) -> bool:
+        pass
+
+    def add(self, k: Any, v: Any) -> None:
+        pass
+
+    def min(self) -> Tuple[Any]:
+        pass
+
+    def remove_min(self) -> Tuple[Any]:
+        pass
+

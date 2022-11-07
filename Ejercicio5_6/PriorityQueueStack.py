2a2f2eb00c0275eb16ee78d4bad849eee3a4b210
Ramiro Buenar <ramirobuenar2000@gmail.com>
2022-11-07 11:51:54 -0300

    Ejercicio 5_6 echo

---
+++ Ejercicio5_6/PriorityQueueStack.py
@@ -0,0 +1,25 @@
+from queue import PriorityQueue
+class Stack:
+    def __init__(self):
+        self.count = 0
+        self.stack = PriorityQueue()
+
+    def push(self, value):
+        self.count -= 1
+        self.stack.put([self.count, value])
+
+    def pop(self):
+        if self.stack.empty():
+            print("Pila Vacia!!")
+            return
+
+        self.count += 1
+        self.stack.get()
+
+    def top(self):
+        temp = self.stack.get()
+        self.stack.put(temp)
+        return temp[1]
+
+    def isEmpty(self):
+        return self.stack.empty()
---
+++ Ejercicio5_6/pq_stack__client.py
@@ -0,0 +1,13 @@
+from Ejercicio5_6.PriorityQueueStack import Stack
+
+pila = Stack()
+print(pila.isEmpty())
+pila.push(5)
+pila.push(4)
+pila.push(3)
+pila.push(2)
+pila.push(1)
+pila.push(0)
+print(pila.isEmpty())
+pila.pop()
+print(pila.top())
